be5fd15cb364314e27d149ad8101c203e3b9be77
snwkng <dmpetlaj@gmail.com>
2024-12-13 15:10:17 +0300

    fix

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -28,12 +28,12 @@ export class UploadController {
   async downloadFile(@Param('uuid') uuid: string) {
     const type = uuid.split('.')[1];
     let fileType = 'image/webp';
-    if (type == 'svg') {
+    if (type === 'svg') {
       fileType = 'image/svg+xml';
-    } else if (type == 'docx' || 'doc') {
+    } else if (type === 'docx' || type === 'doc') {
       fileType =
         'application/vnd.openxmlformats-officedocument.wordprocessingml.document';
-    } else if (type == 'pdf') {
+    } else if (type === 'pdf') {
       fileType = 'application/pdf';
     }
     const response = await this.uploadService.download(uuid);
