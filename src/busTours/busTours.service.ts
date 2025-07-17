662777b1a47f5fa2612f74b7396c92e6a7ff3005
snwkng <dmpetlaj@gmail.com>
2025-07-17 18:20:58 +0300

    hide swagger, fix bugs

--- src/busTours/busTours.service.ts
+++ src/busTours/busTours.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, Error } from 'mongoose';
 import { BusTour } from 'src/busTours/schemas/busTours.schema';
 
 @Injectable()
@@ -15,9 +15,9 @@ export class BusToursService {
     if (params) {
       Object.entries(params).forEach(([key, value]) => {
         if (key === 'city') {
-          query['address.'+key] = value
+          query['address.' + key] = value;
         } else {
-          query[key] = value
+          query[key] = value;
         }
       });
     }
@@ -26,8 +26,16 @@ export class BusToursService {
   }
 
   async getBusTour(id: string) {
-    const hotel = await this.hotelModel.findById(id).exec();
-    return hotel;
+    try {
+      const hotel = await this.hotelModel.findById(id).exec();
+      return hotel;
+    } catch (error) {
+      // все CastError будут отдавать 404
+      if (error instanceof Error.CastError) {
+        throw new NotFoundException(`Invalid ID format: "${id}"`);
+      }
+      throw error; // Re-throw other errors
+    }
   }
 
   async getSeaList(): Promise<string[]> {
@@ -35,8 +43,9 @@ export class BusToursService {
     return seaList;
   }
 
-  async getCitiesList(seaType: string): Promise<string[]> {
-    const cityList: string[] = await this.hotelModel.distinct('address.city', { seaType });
+  async getCitiesList(seaType?: string): Promise<string[]> {
+    const filter = seaType ? { seaType } : {};
+    const cityList: string[] = await this.hotelModel.distinct('address.city', { ...filter });
     return cityList;
   }
 }
